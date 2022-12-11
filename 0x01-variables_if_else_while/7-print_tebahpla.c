d5c0e77b33d8f7b0fd29ebcb73d844ce7c12b503
mbaldeh1 <muhammadoubaldeh@gmail.com>
2022-12-11 13:51:05 -0800

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
+ * * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c = 'z';
 
-	while (c >= 'a')
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
-		putchar(c);
-		c--;
+		putchar(ch);
 	}
-
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
