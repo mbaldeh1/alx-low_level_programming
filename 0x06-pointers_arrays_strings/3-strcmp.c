e0a8f46be4fcec6eac9f30373733de5ddd653141
mbaldeh1 <muhammadoubaldeh@gmail.com>
2022-12-22 03:46:38 -0800

    Task 8

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,46 @@
-#include "main.h"
+
 /**
- * strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
+ * _strcmp - a function that compares two strings
+ *
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
+ *
+ * Return: 0 if s1 and s2 are equal
+ * negative integer if the stopping character
+ * in @s1 was less than the stopping
+ * character in @s2
+ * positive integer if the stopping characterin @s1 was greater than the stopping character in @s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, r;
 
 	/**
-	 * We have reached the end of S1 OR we have reached the end of S2
-	*/
+	 * iterate through src and
+	 *  compare it with dest
+	 */
+	 while (s1[i] != '\0' && s2[i] != '\0')
+	 {
+		 /**
+		  * * if they differ by a single character
+		  * don't iterate further
+		  */
+		  if (s1[i] != s2[i])
+		  {
+			  /*
+			   * return the difference between
+			   * the two characters
+			   */
+			   r = s1[i] - s2[i];
+			   break;
+		  }
+		  else
+		  {
+			  r = s1[i] - s2[i];
+		  }
+		  i++;
+	 }
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-	}
-	return (0);
+	 return (r);
 }
